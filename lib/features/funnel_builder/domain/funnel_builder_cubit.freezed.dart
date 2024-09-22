961954e3a5990c2d307acbd88998cac94c5e88d8
Sokhibjon18 <safarovsoxibjon18@gmail.com>
2024-09-22 07:02:30 +0500

    – some updates

---
+++ lib/features/funnel_builder/domain/funnel_builder_cubit.freezed.dart
@@ -0,0 +1,332 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'funnel_builder_cubit.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+/// @nodoc
+mixin _$FunnelBuilderState {
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() initial,
+    required TResult Function(List<ComponentParams> params) currentScreenParams,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult? Function()? initial,
+    TResult? Function(List<ComponentParams> params)? currentScreenParams,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? initial,
+    TResult Function(List<ComponentParams> params)? currentScreenParams,
+    required TResult orElse(),
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(_Initial value) initial,
+    required TResult Function(_CurrentScreenParams value) currentScreenParams,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult? Function(_Initial value)? initial,
+    TResult? Function(_CurrentScreenParams value)? currentScreenParams,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(_Initial value)? initial,
+    TResult Function(_CurrentScreenParams value)? currentScreenParams,
+    required TResult orElse(),
+  }) =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $FunnelBuilderStateCopyWith<$Res> {
+  factory $FunnelBuilderStateCopyWith(
+          FunnelBuilderState value, $Res Function(FunnelBuilderState) then) =
+      _$FunnelBuilderStateCopyWithImpl<$Res, FunnelBuilderState>;
+}
+
+/// @nodoc
+class _$FunnelBuilderStateCopyWithImpl<$Res, $Val extends FunnelBuilderState>
+    implements $FunnelBuilderStateCopyWith<$Res> {
+  _$FunnelBuilderStateCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  /// Create a copy of FunnelBuilderState
+  /// with the given fields replaced by the non-null parameter values.
+}
+
+/// @nodoc
+abstract class _$$InitialImplCopyWith<$Res> {
+  factory _$$InitialImplCopyWith(
+          _$InitialImpl value, $Res Function(_$InitialImpl) then) =
+      __$$InitialImplCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$$InitialImplCopyWithImpl<$Res>
+    extends _$FunnelBuilderStateCopyWithImpl<$Res, _$InitialImpl>
+    implements _$$InitialImplCopyWith<$Res> {
+  __$$InitialImplCopyWithImpl(
+      _$InitialImpl _value, $Res Function(_$InitialImpl) _then)
+      : super(_value, _then);
+
+  /// Create a copy of FunnelBuilderState
+  /// with the given fields replaced by the non-null parameter values.
+}
+
+/// @nodoc
+
+class _$InitialImpl implements _Initial {
+  const _$InitialImpl();
+
+  @override
+  String toString() {
+    return 'FunnelBuilderState.initial()';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType && other is _$InitialImpl);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() initial,
+    required TResult Function(List<ComponentParams> params) currentScreenParams,
+  }) {
+    return initial();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult? Function()? initial,
+    TResult? Function(List<ComponentParams> params)? currentScreenParams,
+  }) {
+    return initial?.call();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? initial,
+    TResult Function(List<ComponentParams> params)? currentScreenParams,
+    required TResult orElse(),
+  }) {
+    if (initial != null) {
+      return initial();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(_Initial value) initial,
+    required TResult Function(_CurrentScreenParams value) currentScreenParams,
+  }) {
+    return initial(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult? Function(_Initial value)? initial,
+    TResult? Function(_CurrentScreenParams value)? currentScreenParams,
+  }) {
+    return initial?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(_Initial value)? initial,
+    TResult Function(_CurrentScreenParams value)? currentScreenParams,
+    required TResult orElse(),
+  }) {
+    if (initial != null) {
+      return initial(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class _Initial implements FunnelBuilderState {
+  const factory _Initial() = _$InitialImpl;
+}
+
+/// @nodoc
+abstract class _$$CurrentScreenParamsImplCopyWith<$Res> {
+  factory _$$CurrentScreenParamsImplCopyWith(_$CurrentScreenParamsImpl value,
+          $Res Function(_$CurrentScreenParamsImpl) then) =
+      __$$CurrentScreenParamsImplCopyWithImpl<$Res>;
+  @useResult
+  $Res call({List<ComponentParams> params});
+}
+
+/// @nodoc
+class __$$CurrentScreenParamsImplCopyWithImpl<$Res>
+    extends _$FunnelBuilderStateCopyWithImpl<$Res, _$CurrentScreenParamsImpl>
+    implements _$$CurrentScreenParamsImplCopyWith<$Res> {
+  __$$CurrentScreenParamsImplCopyWithImpl(_$CurrentScreenParamsImpl _value,
+      $Res Function(_$CurrentScreenParamsImpl) _then)
+      : super(_value, _then);
+
+  /// Create a copy of FunnelBuilderState
+  /// with the given fields replaced by the non-null parameter values.
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? params = null,
+  }) {
+    return _then(_$CurrentScreenParamsImpl(
+      null == params
+          ? _value._params
+          : params // ignore: cast_nullable_to_non_nullable
+              as List<ComponentParams>,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$CurrentScreenParamsImpl implements _CurrentScreenParams {
+  const _$CurrentScreenParamsImpl(final List<ComponentParams> params)
+      : _params = params;
+
+  final List<ComponentParams> _params;
+  @override
+  List<ComponentParams> get params {
+    if (_params is EqualUnmodifiableListView) return _params;
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableListView(_params);
+  }
+
+  @override
+  String toString() {
+    return 'FunnelBuilderState.currentScreenParams(params: $params)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$CurrentScreenParamsImpl &&
+            const DeepCollectionEquality().equals(other._params, _params));
+  }
+
+  @override
+  int get hashCode =>
+      Object.hash(runtimeType, const DeepCollectionEquality().hash(_params));
+
+  /// Create a copy of FunnelBuilderState
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$CurrentScreenParamsImplCopyWith<_$CurrentScreenParamsImpl> get copyWith =>
+      __$$CurrentScreenParamsImplCopyWithImpl<_$CurrentScreenParamsImpl>(
+          this, _$identity);
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() initial,
+    required TResult Function(List<ComponentParams> params) currentScreenParams,
+  }) {
+    return currentScreenParams(params);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult? Function()? initial,
+    TResult? Function(List<ComponentParams> params)? currentScreenParams,
+  }) {
+    return currentScreenParams?.call(params);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? initial,
+    TResult Function(List<ComponentParams> params)? currentScreenParams,
+    required TResult orElse(),
+  }) {
+    if (currentScreenParams != null) {
+      return currentScreenParams(params);
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(_Initial value) initial,
+    required TResult Function(_CurrentScreenParams value) currentScreenParams,
+  }) {
+    return currentScreenParams(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult? Function(_Initial value)? initial,
+    TResult? Function(_CurrentScreenParams value)? currentScreenParams,
+  }) {
+    return currentScreenParams?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(_Initial value)? initial,
+    TResult Function(_CurrentScreenParams value)? currentScreenParams,
+    required TResult orElse(),
+  }) {
+    if (currentScreenParams != null) {
+      return currentScreenParams(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class _CurrentScreenParams implements FunnelBuilderState {
+  const factory _CurrentScreenParams(final List<ComponentParams> params) =
+      _$CurrentScreenParamsImpl;
+
+  List<ComponentParams> get params;
+
+  /// Create a copy of FunnelBuilderState
+  /// with the given fields replaced by the non-null parameter values.
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  _$$CurrentScreenParamsImplCopyWith<_$CurrentScreenParamsImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
